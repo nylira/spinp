bca2930cf855090d949143f03f95fd48d44e91a9
Peng Zhong <peng@nylira.com>
2011-06-17 04:42:06 +0800

    update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $("dl.tracks").accordion({
   //active: false,
   collapsible: true
 });
+
+$("a.hide.menu").click(function() {
+  $("#spindal").hide('slide', { direction: 'left'});
+  $("a.show.menu").show('slide', { direction: 'left'});
+});
+
+$("a.show.menu").click(function() {
+  $("#spindal").show('slide', { direction: 'left'});
+  $("a.show.menu").hide('slide', { direction: 'left'});
+});
