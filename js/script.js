c7622e2b853346af3f65bb666b8468bfb1ca33e2
Peng Zhong <peng@nylira.com>
2011-07-01 00:11:23 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(".prev-album").click(function() {
-  $("section.album.title h2").removeClass("active").next().hide();
+  $(".album.title h2").removeClass("active").next().hide();
   $(".ui-accordion-header").removeClass("ui-state-active").addClass("ui-state-default");
   $(".ui-accordion-content").removeClass("ui-accordion-content-active").hide();
 });
 $(".next-album").click(function() {
-  $("section.album.title h2").removeClass("active").next().hide();
+  $(".album.title h2").removeClass("active").next().hide();
   $(".ui-accordion-header").removeClass("ui-state-active");
   $(".ui-accordion-content").removeClass("ui-accordion-content-active").hide();
 });
 
-$("section.album.title h2").click(function() {
+$(".album.title h2").click(function() {
   $(this).toggleClass("active");
   $(this).next().slideToggle();
 });
@@ -23,7 +23,7 @@ $(".volume.toggle").click(function() {
   $(this).toggleClass("active");
 });
 
-$("menu.share span.label").click(function() {
+$(".menu.share span.label").click(function() {
   $(this).toggleClass("active");
   $(this).next().toggle();
 });
